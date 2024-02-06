1045f6f0f626ef9bfdf7c488fb8395502624adde
Daniel <daniel@DanMac.local>
2024-02-05 23:22:58 -0500

    merge

--- src/IController.cs
+++ src/IController.cs
@@ -1,10 +1,15 @@
 ﻿/*
  *	Interface for IController
  */
-public interface IController
+public interface IController<T>
 {
 	/*
 	 * Updates the controller and executes a command based off the state
 	 */
 	void Update();
+
+	/*
+	 * Register command to Dictionary associated with T identifier
+	 */
+    public void RegisterCommand(T identifier, ICommand command);
 }
