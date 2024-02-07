f640e919c381f8c4bc9d302fff77864f44646e18
AlphaPizzadog <57466221+AlphaPizzadog@users.noreply.github.com>
2024-02-07 14:54:55 -0500

    Replaced old sprite classes with new player sprite classes

---
+++ src/IdleLeftSprite.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+/*
+ *  SetSprite1 class for non-moving, non-animated sprite
+ */
+public class IdleLeftSprite : ISprite
+{
+    private Texture2D Sprite;
+
+    // Constructor
+    public IdleLeftSprite(Texture2D sprite)
+	{
+        Sprite = sprite;
+    }
+
+    public void Draw(SpriteBatch spriteBatch, Vector2 location)
+    {
+        Rectangle sourceRectangle = new Rectangle(1, 11, 16, 16);
+        Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, 64, 64);
+
+        spriteBatch.Begin();
+        spriteBatch.Draw(Sprite, destinationRectangle, sourceRectangle, Color.White);
+        spriteBatch.End();
+    }
+
+    public void Update()
+    {
+        // No Implementation, nothing needs to be updated
+    }
+
+}
