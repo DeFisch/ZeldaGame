759e5539f8c264560a34259945f78162abfe8797
AlphaPizzadog <57466221+AlphaPizzadog@users.noreply.github.com>
2024-02-08 19:39:18 -0500

    Updated KeyboardController to work with KeyHandler

--- src/KeyboardController.cs
+++ src/KeyboardController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
+using System.Data.Common;
 using ZeldaGame;
 
 /*
@@ -8,28 +9,57 @@ using ZeldaGame;
  */
 public class KeyboardController : IController
 {
-    private Dictionary<Keys, ICommand> controllerMappings;
+    private Dictionary<KeyValuePair<Keys, ICommand>, int> controllerMappings;
+    private KeyboardHandler keyboardHandler;
 
     // Constructor: Initializes the commands based off the keys pressed
     public KeyboardController()
     {
-        controllerMappings = new Dictionary<Keys, ICommand>();
+        controllerMappings = new Dictionary<KeyValuePair<Keys, ICommand>, int>();
+		keyboardHandler = new KeyboardHandler();
     }
 
-    public void RegisterCommand(Keys identifier, ICommand command)
-    {
-        controllerMappings.Add(identifier, command);
-    }
+    public void RegisterCommand(Keys identifier, ICommand command, int commandType) // 0 = isPressed, 1 = isHeld, 2 = isRelease
+	{
+		controllerMappings.Add(new KeyValuePair<Keys, ICommand>(identifier, command), commandType);
+	}
 
-    public void Update()
+	public void Update()
     {
-        Keys[] pressedKeys = Keyboard.GetState().GetPressedKeys();
+        keyboardHandler.Update();
 
-        foreach (Keys key in pressedKeys)
-        {
-            if (controllerMappings.ContainsKey(key)) {
-                controllerMappings[key].Execute();
+        foreach (KeyValuePair<KeyValuePair<Keys, ICommand>, int> keyCommandTypePair in controllerMappings) {
+            switch (keyCommandTypePair.Value) {
+                case 0:
+                    if (keyboardHandler.IsPressed(keyCommandTypePair.Key.Key)) {
+                        Pressed(keyCommandTypePair.Key);
+                    }
+                    break;
+                case 1:
+                    if (keyboardHandler.IsHeld(keyCommandTypePair.Key.Key)) {
+                        Held(keyCommandTypePair.Key);
+                    }
+                    break;
+                case 2:
+                    if (keyboardHandler.IsReleased(keyCommandTypePair.Key.Key)) {
+                        Released(keyCommandTypePair.Key);
+                    }
+                    break;
+                default:
+                    break;
             }
         }
     }
+
+    public void Pressed(KeyValuePair<Keys, ICommand> keyPair) {
+        keyPair.Value.Execute(); 
+    }
+
+	public void Held(KeyValuePair<Keys, ICommand> keyPair) {
+		keyPair.Value.Execute();
+	}
+
+	public void Released(KeyValuePair<Keys, ICommand> keyPair) {
+		keyPair.Value.Execute();
+	}
 }
