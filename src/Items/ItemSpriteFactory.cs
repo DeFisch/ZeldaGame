11f5d357aba7eefc2b1425f7f488a272cd321e1b
omaynard <omaynard2125@gmail.com>
2024-02-11 18:34:31 -0500

    Fixed duplicate ItemSpriteFactory

--- src/Items/ItemSpriteFactory.cs
+++ src/Items/ItemSpriteFactory.cs
@@ -12,8 +12,15 @@ namespace ZeldaGame.Items
     {
         List<ISprite> objectList;
         BlueRuby blueRuby;
+        YellowRuby yellowRuby;
+        Heart heart;
+        HeartContainer heartContainer;
+        Triforce triforce;
+
         Texture2D texture;
         Vector2 pos;
+        private int cycler = 0;
+        private int index = 0;
 
         public ItemSpriteFactory(Texture2D texture)
         {
@@ -21,17 +28,45 @@ namespace ZeldaGame.Items
             this.texture = texture;
             pos = new Vector2(300, 150);
             blueRuby = new BlueRuby(this.texture, pos);
+            yellowRuby = new YellowRuby(this.texture, pos);
+            heart = new Heart(this.texture, pos);
+            triforce = new Triforce(this.texture, pos);
+            heartContainer = new HeartContainer(this.texture, pos);
         }
 
         public void ObjectList()
         {
             objectList.Add(blueRuby);
+            objectList.Add(yellowRuby);
+            objectList.Add(heart);
+            objectList.Add(triforce);
+            objectList.Add(heartContainer);
+
+
 
         }
 
+        public void Cycle(int lastOrNext)
+        {
+            if (lastOrNext == 0) //Cycling backwards
+            {
+                cycler--;
+                if (cycler < 0)
+                {
+                    cycler = objectList.Count - 1;
+                }
+                index = cycler % objectList.Count;
+            }
+            if (lastOrNext == 1)
+            {
+                cycler++;
+                index = cycler % objectList.Count;
+            }
+        }
         public void Draw(SpriteBatch spriteBatch)
         {
-            blueRuby.Draw(spriteBatch, pos);
+
+            objectList[index].Draw(spriteBatch, pos);
         }
     }
 }
