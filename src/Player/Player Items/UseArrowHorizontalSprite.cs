1b49c444f9b611746a63e7222fab29b17c0b4fd2
dperry8 <dlperry008@gmail.com>
2024-02-12 13:14:26 -0500

    Added item usage, all left is animation

---
+++ src/Player/Player Items/UseArrowHorizontalSprite.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+public class UseArrowHorizontalSprite : ISprite
+{
+    SpriteEffects effect;
+    private Texture2D Sprite;
+    private int direction;
+    private int X;
+
+    public UseArrowHorizontalSprite(Texture2D sprite, int direction)
+    {
+        Sprite = sprite;
+        effect = SpriteEffects.None;
+        this.direction = direction;
+    }
+
+    public void Draw(SpriteBatch spriteBatch, Vector2 location)
+    {
+
+        Rectangle sourceRectangle = new Rectangle(10, 185, 15, 15);
+        Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, sourceRectangle.Width * 2, sourceRectangle.Height * 2);
+        if (direction == 1) // 1 indicates left
+        {
+            effect = SpriteEffects.FlipHorizontally;
+        }
+        spriteBatch.Draw(Sprite, destinationRectangle, sourceRectangle, Color.White, rotation: 0, new Vector2(0, 0), effects: effect, 1);
+    }
+
+    public void Update()
+    {
+
+    }
+
+}
--- src/Player/Player Items/UseArrowVerticalSprite.cs
+++ src/Player/Player Items/UseArrowVerticalSprite.cs
@@ -1,21 +1,28 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-public class UseItemLeftSprite : ISprite
+public class UseArrowVerticalSprite : ISprite
 {
+    SpriteEffects effect;
     private Texture2D Sprite;
+    private int direction;
 
-    public UseItemLeftSprite(Texture2D sprite)
+    public UseArrowVerticalSprite(Texture2D sprite, int direction)
     {
         Sprite = sprite;
+        effect = SpriteEffects.None;
+        this.direction = direction;
     }
 
     public void Draw(SpriteBatch spriteBatch, Vector2 location)
     {
 
-        Rectangle sourceRectangle = new Rectangle(124, 11, 16, 16);
+        Rectangle sourceRectangle = new Rectangle(1, 185, 8, 15);
         Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, sourceRectangle.Width * 2, sourceRectangle.Height * 2);
-        SpriteEffects effect = SpriteEffects.FlipHorizontally;
+        if (direction == 1) // 1 indicates down
+        {
+            effect = SpriteEffects.FlipVertically;
+        }
         spriteBatch.Draw(Sprite, destinationRectangle, sourceRectangle, Color.White, rotation: 0, new Vector2(0, 0), effects: effect, 1);
     }
 
