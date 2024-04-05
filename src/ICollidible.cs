6071a811ce8dd46ed60470b1475bc11be557f539
AlphaPizzadog <57466221+AlphaPizzadog@users.noreply.github.com>
2024-04-05 14:54:19 -0400

    Create ICollidible.cs

---
+++ src/ICollidible.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace ZeldaGame;
+/*
+ * Interface for ICollibilde
+ */
+public interface ICollidible {
+	public Rectangle GetHitBox();
+	public void OnCollision(Rectangle intersect);
+}
